b192d190868ee1d6fb71a9c346eee4cabf497b1f
marce <marce@rhel-8.3>
2021-04-10 20:55:21 +0200

    nuevo ejercicio Cap04Ej6.java

---
+++ src/capitulo04/Cap04Ej6.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package capitulo04;
+
+/**
+ * @author marce
+ *
+ */
+public class Cap04Ej6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
