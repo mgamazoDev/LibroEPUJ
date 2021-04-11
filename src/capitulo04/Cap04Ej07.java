f9aee6582e032bf97c21c77bef5b2bb5b2c3de5a
marce <mgamazo.dev@gmail.com>
2021-04-11 11:07:30 +0200

    Comienzo ejercicio 7

---
+++ src/capitulo04/Cap04Ej07.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package capitulo04;
+
+/**
+ * @author marce
+ *
+ */
+public class Cap04Ej07 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
