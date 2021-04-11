8c68628df0ae0a783cd9df178e24c2a5ed350255
marce <mgamazo.dev@gmail.com>
2021-04-11 11:22:22 +0200

    Comienzo con el ejercicio 7

--- src/capitulo04/Cap04Ej07.java
+++ src/capitulo04/Cap04Ej07.java
@@ -1,10 +1,17 @@
 /**
+ * Clase que implementa el ejercicio 6 del capítulo 4, del libro Empezar a Programar
+ * Usando Java. Cuyo enunciado es:
  * 
+ * Escribir una Clase Programa que emplee la clase Hora con los métodos del ejercicio previo
+ * y que en su main escriba la hora actual, pida dos horas al usuario y las escriba en la salida
+ * mostrando además la diferencia en minutos entre ellas.
+ * 
+ * @author marce mgamazo.dev@gmail.com
  */
 package capitulo04;
 
 /**
- * @author marce
+ * 
  *
  */
 public class Cap04Ej07 {
@@ -13,7 +20,7 @@ public class Cap04Ej07 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
